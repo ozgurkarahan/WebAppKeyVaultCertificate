576d62883baa6636092679a3032ff3be302a24d6
Ozgur KARAHAN <ozkara@microsoft.com>
2018-02-06 15:05:17 +0100

    add initial project

---
+++ src/WebAppKeyVault/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using System.Configuration;
+using WebAppKeyVault.Helper;
+
+namespace WebAppKeyVault.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string RedisConnectionString { get; private set; }
+        public async System.Threading.Tasks.Task<ActionResult> Index()
+        {
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var redisUri = ConfigurationManager.AppSettings["redisPrimaryKeySecretUri"];
+            var redisCacheName = ConfigurationManager.AppSettings["redisCacheName"];
+            
+            try
+            {
+                var keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(Tools.GetAccessToken));
+
+                var secret = await keyVaultClient.GetSecretAsync(redisUri).ConfigureAwait(false);
+                ViewBag.Secret = $"Secret: {secret.Value}";
+                RedisConnectionString = string.Format(ConfigurationManager.AppSettings["redisConnectionString"], redisCacheName, secret.Value);
+
+                ViewBag.RedisConnectionString = $"RedisConnectionString: {RedisConnectionString}";
+            }
+            catch (Exception exp)
+            {
+                ViewBag.Error = $"Something went wrong: {exp.Message}";
+            }
+
+            ViewBag.Principal = azureServiceTokenProvider.PrincipalUsed != null ? $"Principal Used: {azureServiceTokenProvider.PrincipalUsed}" : string.Empty;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ src/WebAppKeyVault/Helper/Tools.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
+
+namespace WebAppKeyVault.Helper
+{
+    public static class Tools
+    {
+        public static X509Certificate2 FindCertificateByThumbprint(string findValue)
+        {
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            try
+            {
+                store.Open(OpenFlags.ReadOnly);
+                X509Certificate2Collection col = store.Certificates.Find(X509FindType.FindByThumbprint,
+                    findValue, false); // Don't validate certs, since the test root isn't installed.
+                if (col == null || col.Count == 0)
+                    return null;
+                return col[0];
+            }
+            finally
+            {
+                store.Close();
+            }
+        }
+
+        private static ClientAssertionCertificate _AssertionCert;
+        public static ClientAssertionCertificate AssertionCert {
+            get {
+                if (_AssertionCert == null)
+                {
+                    var clientAssertionCertPfx = FindCertificateByThumbprint(WebConfigurationManager.AppSettings["thumbprint"]);
+                    _AssertionCert = new ClientAssertionCertificate(WebConfigurationManager.AppSettings["clientid"], clientAssertionCertPfx);
+                }
+                return _AssertionCert;
+            }
+        }
+        
+        public static async Task<string> GetAccessToken(string authority, string resource, string scope)
+        {
+            var context = new AuthenticationContext(authority, TokenCache.DefaultShared);
+            var result = await context.AcquireTokenAsync(resource, AssertionCert);
+            return result.AccessToken;
+        }
+    }
+}
